034d444b0567aa2390dfd87820527f8f66745f48
rusafe <150953396+rusafe@users.noreply.github.com>
2024-12-01 22:12:32 +0100

    Added initial SQL script

---
+++ resources/sql/initialScript.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS population;
+
+USE population;
+
+CREATE TABLE users(
+	id INT(10) PRIMARY KEY AUTO_INCREMENT,
+	login VARCHAR(20),
+	password VARCHAR(40),
+	type VARCHAR(6)
+);
+
+INSERT INTO users (login, password, type) VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin');
+
+CREATE USER 'admin' IDENTIFIED BY '21232f297a57a5a743894a0e4a801fc3';
+GRANT ALL PRIVILEGES ON *.* TO 'admin' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
